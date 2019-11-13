98cc33bdb2340951f6b982234a0ecabed7d709a5
Trevor Kirpaul <trevor.kirpaul@rtslabs.com>
2019-11-13 13:05:34 -0500

    - add README and plugin file

---
+++ README.md
@@ -0,0 +1,12 @@
+# rst-zsh
+
+This is a ZSH plugin containing helpful aliases for RTS projects.
+
+## Installation
+
+Antigen:
+
+```
+antigen bundle trevorKirpaulRTS/rts-zsh
+```
+
---
+++ rts-zsh.plugin.zsh
@@ -0,0 +1,8 @@
+#################################
+# ALIASES                       #
+#################################
+
+# sorted alphabetically
+
+alias rts="Hello, this is the first command"
+

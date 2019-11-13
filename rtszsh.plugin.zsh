a4e3803c322aa416fb5bde8cd36212631546eaaa
Trevor Kirpaul <trevor.kirpaul@rtslabs.com>
2019-11-13 13:17:20 -0500

    - add Shane's command for react native clean

--- rtszsh.plugin.zsh
+++ rtszsh.plugin.zsh
@@ -5,4 +5,4 @@
 # sorted alphabetically
 
 alias rts="echo Hello, this is the first command"
-
+alias rnClean="watchman watch-del-all && rm -rf $TMPDIR/react-native-packager-cache-* && rm -rf $TMPDIR/metro-bundler-cache-* && rm -rf node_modules/ && npm cache clean --force & npm install && cd ios && pod install && cd .. && npm run ios"

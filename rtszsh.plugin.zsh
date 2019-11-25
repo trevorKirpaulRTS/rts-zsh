c35b9761a3b7856687bcab568271e26ea2941b1d
Trevor Kirpaul <trevor.kirpaul@rtslabs.com>
2019-11-25 10:18:17 -0500

    - added rnCleanYarn

--- rtszsh.plugin.zsh
+++ rtszsh.plugin.zsh
@@ -5,5 +5,9 @@
 # sorted alphabetically
 
 alias expoClean="watchman watch-del-all && rm -rf node_modules && yarn install && rm -rf /tmp/metro-bundler-cache-* && rm -rf /tmp/haste-map-react-native-packager-* && echo expoClean: Complete"
+
 alias rts="echo Hello, this is the first command"
+
 alias rnClean="watchman watch-del-all && rm -rf $TMPDIR/react-native-packager-cache-* && rm -rf $TMPDIR/metro-bundler-cache-* && rm -rf node_modules/ && npm cache clean --force & npm install && cd ios && pod install && cd .. && npm run ios"
+
+alias rnCleanYarn="watchman watch-del-all && rm -rf $TMPDIR/react-native-packager-cache-* && rm -rf $TMPDIR/metro-bundler-cache-* && rm -rf node_modules/ && yarn cache clean & yarn install && cd ios && pod install && cd .. && yarn ios"

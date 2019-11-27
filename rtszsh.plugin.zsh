17a6c3d9389416a6d5e5d1ddc61bfed7a1189890
Trevor Kirpaul <trevor.kirpaul@rtslabs.com>
2019-11-27 12:32:15 -0500

    - update plugin to include function - add file for help definitions, used in new function

--- rtszsh.plugin.zsh
+++ rtszsh.plugin.zsh
@@ -11,3 +11,21 @@ alias rts="echo Hello, this is the first command"
 alias rnClean="watchman watch-del-all && rm -rf $TMPDIR/react-native-packager-cache-* && rm -rf $TMPDIR/metro-bundler-cache-* && rm -rf node_modules/ && npm cache clean --force & npm install && cd ios && pod install && cd .. && npm run ios"
 
 alias rnCleanYarn="watchman watch-del-all && rm -rf $TMPDIR/react-native-packager-cache-* && rm -rf $TMPDIR/metro-bundler-cache-* && rm -rf node_modules/ && yarn cache clean & yarn install && cd ios && pod install && cd .. && yarn ios"
+
+#################################
+# FUNCTIONS                     #
+#################################
+
+source ./help/definitions
+
+# main function for package which handles help functionality
+rts-zsh() {
+  if [[ $1 == "-h"]]; then
+    if [[ $2 === "rnClean" ]]; then
+      echo HELP_MESSAGE_rnClean
+    fi
+
+    echo "Error: Please enter a command as the second arg"
+    echo "Example: rts-zsh -h rnClean"
+  fi
+}

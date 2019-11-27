f2a0b24580da4dd9397e349657883d4ed8f1331e
Trevor Kirpaul <trevor.kirpaul@rtslabs.com>
2019-11-27 13:30:18 -0500

    - added help commands for other commands

--- rtszsh.plugin.zsh
+++ rtszsh.plugin.zsh
@@ -22,7 +22,19 @@ alias rnCleanYarn="watchman watch-del-all && rm -rf $TMPDIR/react-native-package
 _rts-zsh() {
   if [[ $1 == "-h" ]]; then
     if [[ $2 == "rnClean" ]]; then
-      echo "${CYAN}rnClean \n${PURPLE}This is used to clean your react native project. It will remove packages, clear the cache, ect"
+      echo "${CYAN}rnClean \n${PURPLE}This command can be used to clear cached packages and build artifacts when working with a React Native project. \nThis command will run the following: \n- watchman watch-del-all \n- rm -rf $TMPDIR/react-native-packager-cache-* \n- rm -rf $TMPDIR/metro-bundler-cache-* \n- rm -rf node_modules/ \n- npm cache clean \n- npm install \n- cd ios && pod install && cd .. \n- npm run ios"
+      return
+      exit
+    fi
+
+    if [[ $2 == "rnCleanYarn "]]; then
+      echo "${CYAN}rnCleanYarn \n${PURPLE}This command is exactly like ${CYAN}rnClean ${PURPLE}except it will use the yarn package manager instead of npm"
+      return
+      exit
+    fi
+
+    if [[ $2 == "expoClean "]]; then
+      echo "${CYAN}expoClean \n${PURPLE}This command can be used to clear cached packages and build artifacts when working on an ${CYAN}expo ${PURPLE}project. \nThis command will run the following: \n- watchman watch-del-all \n- rm -rf node_modules \n- yarn install \n- rm -rf /tmp/metro-bundler-cache-* \n- rm -rf /tmp/haste-map-react-native-packager-* \n- echo expoClean: Complete"
       return
       exit
     fi
